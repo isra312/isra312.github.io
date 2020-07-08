7eff6026a4d62b5da0f3337f4ffed06a1851da78
israel <israel.aires@sequor.com.br>
2020-07-07 21:38:18 -0300

    feat: Turn high scores list into a table

--- app/highscores/highscores.js
+++ app/highscores/highscores.js
@@ -1,9 +1,12 @@
-const highScoresList = document.getElementById('highScoresList')
+const highScoresTable = document.getElementById("highScoresTable");
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML =
-    highScores.map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+highScoresTable.innerHTML = highScores
+  .map((score) => {
+    return `<tr class="high-score">
+                <td>${score.name}</td>
+                <td>${score.score}</td>
+            </tr>`;
+  })
+  .join("");
